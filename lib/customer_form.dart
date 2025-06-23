ab42497acaf2448e09c00596d064e21d2323ae95
eddie9802 <ehamilton9810@gmail.com>
2025-06-23 17:00:45 +0100

    Adds a lot of quality of life updates

--- lib/customer_form.dart
+++ lib/customer_form.dart
@@ -13,12 +13,16 @@ class CustomerForm extends StatefulWidget {
 class _CustomerFormState extends State<CustomerForm> {
 
   final List<String> _customerForm = ["Name", "Company", "Contact Number", "Registration Number", "Reason For Visit"];
-  final List<String> _customerFormQuestions = 
-                                              ["What is your name?",
+  final List<String> _customerFormQuestions = [
+                                              "What is your name?",
                                               "What company are you from?",
                                               "What is your contact number?",
                                               "What is your vehicle's registration number?",
-                                              "What is your reason for visiting?"];
+                                              "What is your reason for visiting?"
+                                              ];
+
+  final Map<String, String> _fieldText = {"default": "", "required": "This field is required"};
+  String _currentTextField = "default";
   
   late List<TextEditingController> _controllers;
   int _currentStep = 0;
@@ -54,7 +58,33 @@ class _CustomerFormState extends State<CustomerForm> {
     );
   }
 
+
+  // Checks if the question is not empty
+  void _validateQuestion() {
+    if (_controllers[_currentStep].text.trim().isEmpty) {
+      setState(() {
+        _currentTextField = "required";
+      });
+    } else {
+
+      // Resets the _currentTextField if it was changed
+      if (_currentTextField == "required") {
+        _currentTextField = "default";
+      }
+
+      // Goes to next question if there is another one
+      // else, submit the form
+      if (_currentStep < _customerForm.length - 1){
+        _goToNextQuestion();
+      } else {
+        _submitForm();
+      }
+      
+    }
+  }
+
   void _goToNextQuestion() {
+    FocusScope.of(context).unfocus(); // Puts the keyboard away when the question changes
     setState(() {
       if (_currentStep < _customerForm.length - 1) {
         _currentStep++;
@@ -82,6 +112,21 @@ class _CustomerFormState extends State<CustomerForm> {
   }
 
 
+  // Goes back to the previous question
+  void _goToPreviousQuestion() {
+
+    // Ensures text field label is at the default
+    if (_currentTextField == "required") {
+      _currentTextField = "default";
+    }
+    FocusScope.of(context).unfocus(); // Puts the keyboard away when the question changes
+    setState(() {
+      if (_currentStep > 0) {
+        _currentStep--;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     bool isReasonForVisit = _customerForm[_currentStep] == "Reason For Visit";
@@ -95,9 +140,10 @@ class _CustomerFormState extends State<CustomerForm> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text(_customerFormQuestions[_currentStep]),
+                  Text(_customerFormQuestions[_currentStep],
+                    style: TextStyle(fontSize: 24),),
                   Padding(
-                      padding: EdgeInsets.only(bottom: 40.0),
+                      padding: EdgeInsets.only(bottom: 40.0, top: 40.0),
                       child:
                       SizedBox(
                         width: 800,
@@ -106,25 +152,36 @@ class _CustomerFormState extends State<CustomerForm> {
                           maxLength: isReasonForVisit ? 250 : null,
                           keyboardType: isReasonForVisit ? TextInputType.multiline : null,
                           maxLines: isReasonForVisit ? null : 1,
-                          onSubmitted: (_) => _goToNextQuestion(),
                           decoration: InputDecoration(
-                            //labelText: _customerForm[_currentStep],
+                            labelText: _fieldText[_currentTextField],
+                            labelStyle: TextStyle(color: Colors.red),
                             border: OutlineInputBorder(),
                           ),
                         ),
                       )
                   ),
-                  SizedBox(height: 20),
-              if (_currentStep == _customerForm.length - 1)
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: Text("Submit"),
-                )
-              else
-                ElevatedButton(
-                  onPressed: _goToNextQuestion,
-                  child: Text("Next"),
+                Text(
+                  "Question ${_currentStep + 1} of ${_customerFormQuestions.length}",
+                  style: TextStyle(fontSize: 18),
                 ),
+                SizedBox(height: 20),
+                Center(
+                  child:
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ElevatedButton(
+                          onPressed: _currentStep == 0 ? null : _goToPreviousQuestion,
+                          child: Text("Back", style: TextStyle(fontSize: 24)),
+                        ),
+                        SizedBox(width: 40),
+                        ElevatedButton(
+                          onPressed: _validateQuestion,
+                          child: Text( _currentStep == _customerForm.length - 1 ? "Submit" : "Next", style: TextStyle(fontSize: 24)),
+                        )
+                      ],
+                    ),
+                  )
               ],
             ),
           ),

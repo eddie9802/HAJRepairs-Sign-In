460a1a1866f350cc4b8341fae124c6b5e3c2cc95
eddie9802 <ehamilton9810@gmail.com>
2025-06-25 09:51:51 +0100

    Adds a Customer class file

---
+++ lib/customer.dart
@@ -0,0 +1,23 @@
+class Customer {
+  final String registration;
+  final String company;
+  final String driverName;
+  final int driverNumber;
+  final String reasonForVisit;
+  final String date;
+  final String signIn;
+  final String signOut;
+
+
+
+  Customer({
+    required this.registration,
+    required this.company,
+    required this.driverName,
+    required this.driverNumber,
+    required this.reasonForVisit,
+    required this.date,
+    required this.signIn,
+    required this.signOut
+    });
+}
